3d7ac720dffa4320dd3e77f0afee5a95dbba8efa
dimitrich212003 <ytrewq212003@mail.ru>
2024-03-06 00:15:19 +0300

    complete main page

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,6 +6,12 @@ import { flsModules } from "./modules.js";
 const burgerIcon = document.querySelector(".header__burger");
 const nav = document.querySelector(".header__nav");
 const logo = document.querySelector(".header__logo");
+const tel = document.querySelectorAll(".tel");
+const form = document.querySelector(".main__contact-form");
+const btnsScroll = document.querySelectorAll('.btnScroll');
+const contact = document.getElementById("contact");
+
+
 const img = document.createElement("img");
 img.src = "./img/logoForBurger.png";
 img.className = "header__logo-img-active";
@@ -38,3 +44,68 @@ actionSpan.addEventListener("mouseover", () => {
 actionSpan.addEventListener("mouseout", () => {
   actionPopup.classList.remove("showActionPopup");
 });
+
+window.addEventListener("DOMContentLoaded", function () {
+  [].forEach.call(tel, function (input) {
+    let keyCode;
+    function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      let pos = this.selectionStart;
+      if (pos < 3) event.preventDefault();
+      let matrix = "+7 (___) ___ ____",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, ""),
+        new_value = matrix.replace(/[_\d]/g, function (a) {
+          return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+        });
+      i = new_value.indexOf("_");
+      if (i != -1) {
+        i < 5 && (i = 3);
+        new_value = new_value.slice(0, i);
+      }
+      let reg = matrix
+        .substr(0, this.value.length)
+        .replace(/_+/g, function (a) {
+          return "\\d{1," + a.length + "}";
+        })
+        .replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+      if (
+        !reg.test(this.value) ||
+        this.value.length < 5 ||
+        (keyCode > 47 && keyCode < 58)
+      )
+        this.value = new_value;
+      if (event.type == "blur" && this.value.length < 5) this.value = "";
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false);
+  });
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let enteredValue;
+  tel.forEach((el) => {
+    enteredValue = el.value;  
+  });
+  console.log("Отправленное значение:", enteredValue);
+});
+
+
+btnsScroll.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    contact.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
+
+
+
